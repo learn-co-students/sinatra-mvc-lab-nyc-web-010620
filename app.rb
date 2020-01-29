dd9141e9af15c0e57923e4c88acd31520b58247a
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-28 20:30:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
-
+# binding.pry
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        @pl = PigLatinizer.new
+        @words = params[:user_phrase]
+
+        erb :piglatin
+    end
+
 end
